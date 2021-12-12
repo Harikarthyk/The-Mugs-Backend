f8bc80e41dd9c49d9fad62eb5481b47c8d8ff125
Harikarthyk <hari.jsmith494@gmail.com>
2021-12-12 16:57:24 +0530

    Transaction controller updated .

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -1,7 +1,23 @@
 const Transaction = require("../models/transaction");
 
 exports.createTransaction = async(req, res) => {
+    try{
 
+        const transaction = await Transaction.create(
+            req.body
+        );
+
+        return res.status(201).json({
+            success: true,
+            transaction
+        });
+
+    }catch(error){
+        return res.status(500).json({
+            success: false,
+            error: error.message || error
+        });
+    }
 }
 
 exports.updateTransaction = async(req, res) => {
