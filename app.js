8a7df1d3287f6e9cfb8f90544aefa0122ed90192
Harikarthyk <hari.jsmith494@gmail.com>
2021-12-27 18:36:19 +0530

    origin error fixed .

--- app.js
+++ app.js
@@ -23,12 +23,7 @@ app.use(fileUploader());
 app.use(cookieParser());
 const corsOptions = {
     //To allow requests from client
-    origin: [
-      "http://localhost:3000",
-      "http://127.0.0.1",
-      "http://104.142.122.231",
-      "https://mugs07.netlify.app/"
-    ],
+    origin: "*",
     credentials: true,
     exposedHeaders: ["set-cookie"],
   };
