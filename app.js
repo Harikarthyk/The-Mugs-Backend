5036a969f9a635ab98f52744780051f08d626840
Harikarthyk <hari.jsmith494@gmail.com>
2021-12-27 18:23:53 +0530

    cors updated .

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const corsOptions = {
       "http://localhost:3000",
       "http://127.0.0.1",
       "http://104.142.122.231",
+      "https://mugs07.netlify.app/"
     ],
     credentials: true,
     exposedHeaders: ["set-cookie"],
